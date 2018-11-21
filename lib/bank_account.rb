c1cedfa214d38816d41f225b9c52ac78b46f9f55
Joseph Corona <josed.hc96@gmail.com>
2018-11-21 20:34:15 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,4 +5,21 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+
+  attr_reader :name
+  attr_accessor :balance, :balance
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid
+    @balance > 0 && @status == "open"
+  end
+
+  
 end
